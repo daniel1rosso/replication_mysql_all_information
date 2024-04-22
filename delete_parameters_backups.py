a56bab53d389c9724498497496a2fa1e80088e85
Daniel Rosso <drosso@startuniversity.net>
2024-04-22 19:53:08 -0300

    Update processes delete_parameters

--- delete_parameters_backups.py
+++ delete_parameters_backups.py
@@ -1,17 +1,19 @@
 import os
+import re
 
 # Directorio que contiene los archivos .sql
 directorio = "backups"
 
-# Función para escanear los archivos .sql en el directorio y eliminar la línea que contiene ROW_FORMAT=FIXED
+# Función para escanear los archivos .sql en el directorio y eliminar la parte de la línea que contiene ROW_FORMAT=FIXED
 def eliminar_row_format_fixed(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
     with open(filename, 'w', encoding='utf-8') as file:
         for line in lines:
-            if "ROW_FORMAT=FIXED" not in line:
-                file.write(line)
+            # Utilizamos expresión regular para encontrar y reemplazar ROW_FORMAT=FIXED
+            line = re.sub(r'ROW_FORMAT=FIXED', '', line)
+            file.write(line)
 
 # Escanea todos los archivos .sql en el directorio
 for filename in os.listdir(directorio):
@@ -19,8 +21,8 @@ for filename in os.listdir(directorio):
         file_path = os.path.join(directorio, filename)
         try:
             eliminar_row_format_fixed(file_path)
-            print(f"Se ha eliminado la línea 'ROW_FORMAT=FIXED' en el archivo {filename}")
-        except:
-            print(f"Error eliminado la línea 'ROW_FORMAT=FIXED' en el archivo {filename}")
+            print(f"Se ha eliminado 'ROW_FORMAT=FIXED' en el archivo {filename}")
+        except Exception as e:
+            print(f"Error eliminando 'ROW_FORMAT=FIXED' en el archivo {filename}: {e}")
 
 print("Proceso completado.")
